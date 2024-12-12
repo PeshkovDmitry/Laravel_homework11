ffec3fd437b1a56dd0350339debd21fd9fe1b17b
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-12-12 17:37:06 +0300

    Готово

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UsersController extends Controller
+{
+    public function index() {
+        $this->authorize('view-any', User::class);
+        return response()->json(User::all());
+    }
+}
